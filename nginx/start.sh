cffa2fbe252b04eae195eb870250515897336f99
Raymond DeVries <raymond.l.devries@gmail.com>
2020-01-20 01:38:21 -0800

    Certbot config attempt 7.

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,6 +2,6 @@
 
 service nginx start
 service nginx status
-certbot --nginx --test-cert --keep-until-expiring --email $EMAIL --agree-tos -n -d skagit60.com -d www.skagit60.com
+certbot --nginx --test-cert --keep-until-expiring --email $EMAIL --agree-tos -n -d skagit60.com
 service nginx stop
 nginx -g 'daemon off;'
