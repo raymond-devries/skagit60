70f47ea84ae385f7565aff18d00dc503b4c60343
Raymond DeVries <raymond.l.devries@gmail.com>
2020-02-04 00:52:52 -0800

    Tested docker configuration for nginx logging.

--- nginx/start.sh
+++ nginx/start.sh
@@ -5,7 +5,7 @@ FILE=/etc/nginx/conf.d/default.conf
 if test -f "$FILE"; then
   echo "default.conf exists, therfore it will be removed"
   rm "$FILE"
-  cp /home/nginx.conf /etc/nginx/conf.d/
+  cp /home/conf.d/nginx.conf /etc/nginx/conf.d/
 else
   echo "default.conf not found original nginx.conf will be kept"
 fi
